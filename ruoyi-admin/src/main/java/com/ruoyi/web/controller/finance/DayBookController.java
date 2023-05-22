fa3c83435bdc13d891c2f30893c5fc2e7925cb1f
张 文涛 <zhangwentao@pxsemic.com>
2023-05-22 17:16:50 +0800

    Initial commit

---
+++ ruoyi-admin/src/main/java/com/ruoyi/web/controller/finance/DayBookController.java
@@ -0,0 +1,15 @@
+package com.ruoyi.web.controller.finance;
+
+import com.ruoyi.common.core.controller.BaseController;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+/**
+ * 财务管理
+ *
+ * @author ruoyi
+ */
+@Controller
+@RequestMapping("/finance/daybook")
+public class DayBookController extends BaseController {
+}
